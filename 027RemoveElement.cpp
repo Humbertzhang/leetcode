9dc5483d4a1041e676cc8b27fbcb6f85eb13a590
Humbertzhang <504490160@qq.com>
2017-09-18 10:35:28 +0800

    027RemoveElement.cpp

---
+++ 027RemoveElement.cpp
@@ -0,0 +1,19 @@
+/*Your runtime beats 62.34 % of cpp submissions*/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0, count = 0;
+        int size = nums.size();
+        
+        for(i = 0&&count; count < nums.size(); ++count){
+            if(nums[i] == val){
+                for(int j = i; j < nums.size() - 1; j++)
+                    nums[j] = nums[j+1];
+                size -= 1;
+            }
+            else
+                i += 1;
+        }
+        return size;
+    }
+};

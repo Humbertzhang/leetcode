1816b548f6367f50b1e593bc9e11f802f77f3cb4
Humbertzhang <504490160@qq.com>
2018-04-10 16:42:58 +0800

    015 3 sum

--- 015_3sum.cpp
+++ 015_3sum.cpp
@@ -3,15 +3,18 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ret;
         ret.reserve(200);
-        
         if(nums.size() <= 2){
             return ret;
         }
         
         int vsize = nums.size();
+        
         sort(nums.begin(), nums.end());
         
-        for(int i = 0; i <= vsize-3; i++){
+        for(int i = 0; i < vsize-2; i++){
+            //防止重复
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            
             int a = nums[i];
             int start = i + 1;
             int end = vsize - 1;
@@ -25,7 +28,15 @@ public:
                     v.push_back(b);
                     v.push_back(c);
                     ret.push_back(v);
+                    //以下代码为了防止重复
+                    start+=1;
                     end -= 1;
+                    while(nums[start] == nums[start-1]){
+                        start += 1;
+                    }
+                    while(nums[end] == nums[end+1]){
+                        end -= 1;
+                    }
                 }
                 else if(a + b + c > 0){
                     end -= 1;
@@ -35,8 +46,8 @@ public:
                 }
             }
         }
-        vector<vector<int>>::iterator it = unique(ret.begin(), ret.end());
-        ret.resize( distance(ret.begin(), it) );
+        //vector<vector<int>>::iterator it = unique(ret.begin(), ret.end());
+        //ret.resize( distance(ret.begin(), it) );
         return ret;
     }
 };

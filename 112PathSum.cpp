ec62773f375188e8d5218ab58945f11a0933c757
Humbertzhang <504490160@qq.com>
2018-09-26 13:36:40 +0800

    REWRITE:112PathSum.cpp

--- 112PathSum.cpp
+++ 112PathSum.cpp
@@ -34,3 +34,48 @@ public:
         }
     }
 };
+
+/*2018-9-26 rewrite
+ * */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int globalsum;
+    bool hasPathSum(TreeNode* root, int sum) {
+        globalsum = 0;
+        globalsum = sum;
+        int val = 0;
+        if(root == NULL) {
+            return false;
+        } else {
+            return judgesum(root, val);
+        }   
+    }
+    
+    bool judgesum(TreeNode* root, int val) {
+        if(root-> right == NULL && root->left == NULL) {
+            if(val + root->val == globalsum) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        else if (root->right == NULL && root->left != NULL) {
+            return judgesum(root->left, val+root->val);
+        }
+        else if (root->right != NULL && root->left == NULL) {
+            return judgesum(root->right, val+root->val);
+        }
+        else {
+            return judgesum(root->left, val+root->val) || judgesum(root->right, val+root->val);
+        }
+    }
+};

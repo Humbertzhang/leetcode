361f88e0a90f901cd08ea11fccfdca0fdb0af9e0
Humbertzhang <504490160@qq.com>
2018-09-26 11:20:46 +0800

    RE:101SymmetricTree.cpp

--- 101SymmetricTree.cpp
+++ 101SymmetricTree.cpp
@@ -25,3 +25,54 @@ public:
         }
     }
 };
+
+
+
+/*Solution at 2018-9-26  the fastest*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) {
+            return true;
+        }   
+        if (root->right == NULL && root->left == NULL){
+            return true;
+        }
+        else if (root->right != NULL && root->right != NULL) {
+            return judge(root->left, root->right);
+        }
+        else {
+            return false;
+        }
+    }
+    
+    bool judge(TreeNode* leftnode, TreeNode* rightnode) {
+        if(leftnode == NULL && rightnode == NULL) {
+            return true;
+        }
+        else if (leftnode != NULL && rightnode != NULL) {
+            if (leftnode->val == rightnode->val) {
+                bool ret = judge(leftnode->left, rightnode->right);
+                if(ret) {
+                    return judge(leftnode->right, rightnode->left);
+                } else {
+                    return ret;
+                }
+            } else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+};

f2fef950d73b329384221d870ec580afe4d855a5
Humbertzhang <504490160@qq.com>
2018-11-26 09:27:12 +0800

    [数组]027RemoveElement.cpp

--- 027RemoveElement.cpp
+++ 027RemoveElement.cpp
@@ -1,3 +1,28 @@
+/*2018-11-26
+ *100%
+ * */
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = nums.size();
+        if(count == 0) {
+            return 0;
+        }
+        for(int i = 0; i < count; ) {
+            if(nums[i] == val) {
+                for(int j = i; j < nums.size()-1; j++) {
+                    swap(nums[j], nums[j+1]);
+                }
+                count -= 1;
+            }
+            else {
+                i += 1;
+            }
+        }
+        return count;
+    }
+};
+
 /*Your runtime beats 62.34 % of cpp submissions*/
 class Solution {
 public:

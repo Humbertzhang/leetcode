72697ef94622d84776aa8eaa7fd0edb58d1e32aa
Humbertzhang <504490160@qq.com>
2018-12-31 00:34:09 +0800

    .

--- 062UniquePaths.cpp
+++ 062UniquePaths.cpp
@@ -1,3 +1,38 @@
+/*
+ *dp solution
+ 2018-12-31
+ * */
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        /*
+        状态：a[i][j]，表示到a[i][j]的方法
+        转移：a[i][j] = a[i-1][j] + a[i][j-1]
+        */
+        if(m < 1 || n < 1) {
+            return 0;
+        }
+        if(m == 1 || n == 1) {
+            return 1;
+        }
+        int a[m][n]={0};
+        for(int i = 0; i < m; i++) {
+            a[i][0] = 1;
+        }
+        for(int j = 0; j < n; j++) {
+            a[0][j] = 1;
+        }
+        a[0][0] = 0;
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                a[i][j] = a[i-1][j] + a[i][j-1];
+            }
+        }
+        return a[m-1][n-1];
+    }
+};
+
+
 /**
 用map来存放已经递归中之前计算过的东西
 避免重复计算
--- 413ArithmeticSlices.cpp
+++ 413ArithmeticSlices.cpp
@@ -24,3 +24,29 @@ public:
         return sum;
     }
 };
+
+/*
+ *注意此处的dp[i]记录的为增加了最后数字所增加的增量
+ * */
+
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        // 如果 1 2 3 4 为等差数列了，[1,2,3,4,5]只比[1,2,3,4]多了一个元素【5】，在不包括[1,2,3,4]的前提下，多了[2,3,4,5]这个等差数列的数量为3（等于[1,2,3,4]的数量，为3），还有[1,2,3,4,5]这个本身的数量。
+        // 所以如果A[i] - A[i-1] == A[i-1]-A[i-2], 则dp[i] = dp[i-1] + 1. 此处的dp[i]为增加了5这个数字造成的**增量**.
+        if(A.size() <= 2) {
+            return 0;
+        }
+        int dp[A.size()] = {0};
+        for(int i = 2; i < A.size(); i++) {
+            if (A[i]-A[i-1] == A[i-1]-A[i-2]) {
+                dp[i] = dp[i-1] + 1;
+            }
+        }
+        int sum = 0;
+        for(int i = 0; i < A.size(); i++) {
+            sum += dp[i];
+        }
+        return sum;
+    }
+};
